80e756da70d881676a5fd1c2e2010449f33aa71d
Lucas <Lukas656>
2023-08-18 16:43:45 -0300

    Subindo Projeto

---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import './Button.css'
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (props)=>{
+    return(
+        <button className="button">0</button>
+    )
+}
