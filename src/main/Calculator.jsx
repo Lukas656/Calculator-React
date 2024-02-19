d81c6bc4014c875d5a2a7db6a94ec75c89cff9ce
Lukas656 <le.lu.ka2018@gmail.com>
2024-02-18 23:18:42 -0300

    .

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -27,34 +27,33 @@ export default class Calculator extends Component {
     clearMemory() {
         this.setState({ ...initialState })
     };
-
     setOperation(operation) {
         if (this.state.current === 0) {
             this.setState({ operation, current: 1, clearDisplay: true })
         } else {
-            const equals = operation === '='
-            const currentOperation = this.state.operation
-
-            const values = [...this.state.values]
+            const equals = operation === '=';
+            const currentOperation = this.state.operation;
+    
+            const values = [...this.state.values];
             try {
-                // Evite usar eval para segurança e desempenho
-                values[0] = Function(`'use strict'; return ${values[0]} ${currentOperation} ${values[1]}`)()
+                const expression = `${values[0]} ${currentOperation} ${values[1]}`;
+                const result = new Function(`return (${expression})`)();
+                values[0] = result;
             } catch (error) {
-                values[0] = this.state.values[0]
+                values[0] = this.state.values[0];
             }
-
-            values[1] = 0
-
+    
+            values[1] = 0;
+    
             this.setState({
                 displayValue: String(values[0]),
                 operation: equals ? null : operation,
                 current: equals ? 0 : 1,
                 clearDisplay: !equals,
-                values
-            })
+                values,
+            });
         }
     };
-
     addDigit(n) {
         if (n === '.' && this.state.displayValue.includes('.')) {
             return
